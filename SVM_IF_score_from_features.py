0bb80107cf6bf806ff5527a92928765b91e1b30d
riakanjilal <98623881+riakanjilal@users.noreply.github.com>
2024-03-12 12:47:44 -0400

    Update SVM_IF_score_from_features.py

--- SVM_IF_score_from_features.py
+++ SVM_IF_score_from_features.py
@@ -12,28 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import OneClassSVM
 from sklearn.ensemble import IsolationForest
 
-def perform_one_class_svm(input_file):
-    dt = pd.read_csv(input_file)
-    dt_noinf = dt[~dt.isin([np.nan, np.inf, -np.inf]).any(1)]
-
-    X = dt_noinf.iloc[:, 2:]
-
-    # Normalization
-    scaler = StandardScaler()
-    X_scaler = scaler.fit_transform(X)
-
-    clf = OneClassSVM(gamma='auto').fit(X_scaler)
-    anomaly_scores_svm = clf.score_samples(X_scaler)
-
-    value = dt_noinf.iloc[:, 0:2].values
-    anomaly_scores_svm1 = anomaly_scores_svm.reshape(anomaly_scores_svm.shape[0], 1)
-
-    x_new = np.concatenate((value, anomaly_scores_svm1), axis=1)
-
-    datafm = pd.DataFrame(x_new, columns=("Chr", "Pos", "OCSVM_score"))
-    return datafm
-
-def perform_isolation_forest(input_file):
+def isolation_forest(input_file):
     dt = pd.read_csv(input_file)
     dt_noinf = dt[~dt.isin([np.nan, np.inf, -np.inf]).any(1)]
 
@@ -49,27 +28,61 @@ def perform_isolation_forest(input_file):
     value = dt_noinf.iloc[:, 0:2].values
     anomaly_scores_if1 = anomaly_scores_if.reshape(anomaly_scores_if.shape[0], 1)
 
-    x_new2 = np.concatenate((value, anomaly_scores_if1), axis=1)
+    x_new = np.concatenate((value, anomaly_scores_if1), axis=1)
 
-    datafm2 = pd.DataFrame(x_new2, columns=("Chr", "Pos", "IF_score"))
+    datafm = pd.DataFrame(x_new, columns=("Chr", "Pos", "IF_score"))
+    return datafm
+
+def oneclass_svm_minibatchtraining(input_file, num_sets, interval_size):
+    dt = pd.read_csv(input_file)
+    dt_noinf = dt[~dt.isin([np.nan, np.inf, -np.inf]).any(1)]
+
+    X = dt_noinf.iloc[:, 2:]
+
+    # Normalization
+    scaler = StandardScaler()
+    X_scaler = scaler.fit_transform(X)
+    
+    sets = [X_scaler.iloc[i::num_sets] for i in range(num_sets)]
+
+    clf = OneClassSVM(kernel = 'rbf', gamma='auto')
+    
+    # Train the One-Class SVM model using mini-batches
+    for i, mini_batch in enumerate(sets):
+        start_index = i
+        mini_batch_subset = mini_batch.iloc[start_index::interval_size]  #### sampling in interval_size
+        clf.fit(mini_batch_subset)
+    
+    anomaly_scores_svm = clf.score_samples(X_scaler)
+
+    value = dt_noinf.iloc[:, 0:2].values
+    anomaly_scores_svm2 = anomaly_scores_svm.reshape(anomaly_scores_svm.shape[0], 1)
+
+    x_new2 = np.concatenate((value, anomaly_scores_svm2), axis=1)
+
+    datafm2 = pd.DataFrame(x_new2, columns=("Chr", "Pos", "OCSVM_score"))
     return datafm2
 
 def main():
     input_moments = "moment_features.csv"
     input_fda = "fda_features.csv"
     
-    scoresvm_mom = perform_one_class_svm(input_moments)
-    scoreif_mom = perform_isolation_forest(input_moments)
-    scoreif_fda = perform_isolation_forest(input_fda)
+    scoreif_mom = isolation_forest(input_moments)
+    scoreif_fda = isolation_forest(input_fda)
     
-    output_moments_svm = "SVM_score_from_moments.csv"
+    num_sets = 18
+    interval_size = 10
+    scoresvm_mom = oneclass_svm_minibatchtraining(input_moments, num_sets, interval_size)
+    scoresvm_fda = oneclass_svm_minibatchtraining(input_fda, num_sets, interval_size)
+    
     output_moments_if = "IF_score_from_moments.csv"
     output_fda_if = "IF_score_from_fda.csv"
+    output_moments_svm = "SVM_score_from_moments.csv"
+    output_fda_svm = "SVM_score_from_fda.csv"
     
-    scoresvm_mom.to_csv(output_moments_svm, index=None)
     scoreif_mom.to_csv(output_moments_if, index=None)
     scoreif_fda.to_csv(output_fda_if, index=None)
+    scoresvm_mom.to_csv(output_moments_svm, index=None)
+    scoresvm_fda.to_csv(output_fda_svm, index=None)
 
-# Run the main function
 main()
-
