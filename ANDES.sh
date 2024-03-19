e40ab286fbe5fae4355b722565ba4a1de7b31706
riakanjilal <98623881+riakanjilal@users.noreply.github.com>
2024-03-19 13:55:16 -0400

    Update ANDES.sh

--- ANDES.sh
+++ ANDES.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# run 'chmod +x ANDES.sh' in terminal to give execute permissions to 'ANDES.sh'
-
-# Then run './ANDES.sh' on terminal
-
-# Call the Python script for generating summary statistics and moment features. The user can provide multiple vcf files as 'python ./vcf_ss_M_features.py --vcf CEU21.vcf CEU22.vcf'
+## Command #1
 python ./vcf_ss_M_features.py --vcf "$1"
 
-# Call the R script for generating FDA features
+## Command #2
 Rscript ./ss_fda_features.R
 
-# Call the R script for generating MD-M and MD-F anomaly scores
+## Command #3
 Rscript ./MD_MF_scores.R
 
-# Call the Python script for training IF and SVM from moment and FDA features
+## Command #4
 python ./IFSVM_training.py
 
-# Call the R script for generating IF-M, IF-F, SVM-M, and SVM-F anomaly scores
+## Command #5
 Rscript ./IFSVM_MF_anomalyscores.R
 
